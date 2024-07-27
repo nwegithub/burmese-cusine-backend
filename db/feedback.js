3154619f952e4a2b0d591a715d1ed2928c2e0884
Tin Nwe Aye <linnwe3487@gmail.com>
2024-07-27 14:19:03 +0630

    deleted photo

--- db/feedback.js
+++ db/feedback.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FeedbackSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   comment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
 });
