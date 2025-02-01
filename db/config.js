d16e263464846a45a431dfa40ba3aa01546340db
Mywe <user@users-MacBook-Air.local>
2025-02-01 08:42:16 +0630

    changes

--- db/config.js
+++ db/config.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`)
-
 mongoose.connect(`mongodb+srv://burmeseCuisineUser:burmeseCuisine2024@cluster0.vxvffnj.mongodb.net/${process.env.DB_NAME}`)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
@@ -9,6 +7,16 @@ mongoose.connect(`mongodb+srv://burmeseCuisineUser:burmeseCuisine2024@cluster0.v
   .catch((error) => {
     console.error('Connection error:', error);
   });
+
+// mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`)
+
+// mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`)
+//   .then(() => {
+//     console.log('Connected to local MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Connection error:', error);
+//   });
   const { MongoClient } = require('mongodb');
 const client = new MongoClient('mongodb+srv://username:password@cluster.mongodb.net/myDatabase', {
   // ssl: true,
